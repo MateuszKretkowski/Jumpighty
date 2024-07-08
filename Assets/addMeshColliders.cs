be7ff7d301881162767c568a18c8254f34b96d0f
MateuszKretkowski <mkretkowski13@gmail.com>
2024-07-08 13:11:27 +0200

    HouseProps, Materials, script for adding every mesh collider

--- Assets/addMeshColliders.cs
+++ Assets/addMeshColliders.cs
@@ -10,16 +10,12 @@ public class addMeshColliders : MonoBehaviour
     {
         if (houseProps != null)
         {
-            // Pobierz wszystkie dzieci obiektu houseProps
             foreach (Transform child in houseProps.transform)
             {
-                // SprawdŸ, czy obiekt ma komponent MeshRenderer (aby upewniæ siê, ¿e to jest obiekt 3D)
                 if (child.GetComponent<MeshRenderer>() != null)
                 {
-                    // SprawdŸ, czy obiekt nie ma jeszcze komponentu MeshCollider
                     if (child.GetComponent<MeshCollider>() == null)
                     {
-                        // Dodaj komponent MeshCollider do obiektu
                         child.gameObject.AddComponent<MeshCollider>();
                     }
                 }
