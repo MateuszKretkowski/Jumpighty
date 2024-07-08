a5c50ce2127cd4c6ac622e124ae075b24cf6cdb2
MateuszKretkowski <mkretkowski13@gmail.com>
2024-07-08 17:55:08 +0200

    Materials

---
+++ Assets/Scripts/PixelateMaterial.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelateMaterial : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int pixels = 10;
+
+    [SerializeField] private Shader shader;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
